361cfde0100bb6198a9fcae05fd85ad768108877
jmjmjames <chungjm0711@gmail.com>
2022-07-22 13:54:42 +0900

    first commit

---
+++ src/main/java/com/ssu/travel/MtravelApplication.java
@@ -0,0 +1,13 @@
+package com.ssu.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssu/travel/MtravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssu.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
