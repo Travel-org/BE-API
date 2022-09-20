f4cba91690564c90ee122de84aa49449bafab059
jmjmjames <chungjm0711@gmail.com>
2022-09-20 22:33:21 +0900

    [Feat] Security JwtAuthenticationFilter #9

--- src/main/java/com/ssu/travel/security/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/ssu/travel/security/jwt/filter/JwtAuthenticationFilter.java
@@ -23,11 +23,12 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+
         if (token != null && jwtTokenProvider.validateToken(token)) {
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
+
         chain.doFilter(request, response);
     }
-
 }
