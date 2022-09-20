dceb6c230ff924c12323bb6d541fe1324b571835
jmjmjames <chungjm0711@gmail.com>
2022-09-20 22:36:20 +0900

    [Feat] OAuth Service (#9)

--- src/main/java/com/ssu/travel/security/oauth2/AuthService.java
+++ src/main/java/com/ssu/travel/security/oauth2/AuthService.java
@@ -3,7 +3,7 @@ package com.ssu.travel.security.oauth2;
 import com.ssu.travel.common.code.ErrorCode;
 import com.ssu.travel.common.exception.InvalidPasswordException;
 import com.ssu.travel.common.exception.RecordNotFoundException;
-import com.ssu.travel.security.dto.AuthorizationKakao;
+import com.ssu.travel.security.dto.AuthorizationKakaoDto;
 import com.ssu.travel.security.jwt.provider.JwtTokenProvider;
 import com.ssu.travel.security.oauth2.dto.requset.EmailPasswordRequest;
 import com.ssu.travel.security.oauth2.dto.response.LoginSuccessResponse;
@@ -24,23 +24,21 @@ public class AuthService {
     private final JwtTokenProvider jwtTokenProvider;
 
     public JSONObject kakaoAuthentication(String origin, String code) {
-        AuthorizationKakao authorizationKakao = oauth2Service.callTokenApi(origin, code);
-        JSONObject userInfoFromKakao = oauth2Service.callGetUserByAccessToken(authorizationKakao.getAccess_token());
-        return oauth2Service.setSessionOrRedirectToSignUp(userInfoFromKakao, authorizationKakao.getAccess_token());
+        AuthorizationKakaoDto authorizationKakaoDto = oauth2Service.callTokenApi(origin, code);
+
+        JSONObject userInfoFromKakao = oauth2Service.callGetUserByAccessToken(authorizationKakaoDto.getAccess_token());
+        return oauth2Service.setSessionOrRedirectToSignUp(userInfoFromKakao, authorizationKakaoDto.getAccess_token());
     }
 
     public LoginSuccessResponse login(EmailPasswordRequest request) {
-        User user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new RecordNotFoundException(
-                        "해당 이메일을 가진 사용자를 찾을 수 없습니다",
-                        ErrorCode.USER_NOT_FOUND
-                ));
+        User user = userRepository.findByEmail(request.getEmail()).orElseThrow(() ->
+                new RecordNotFoundException("해당 이메일을 가진 사용자를 찾을 수 없습니다", ErrorCode.USER_NOT_FOUND)
+        );
+
         if (!user.getPassword().equals(request.getPassword())) {
-            throw new InvalidPasswordException(
-                    "잘못된 비밀번호 입니다.",
-                    ErrorCode.INVALID_PASSWORD
-            );
+            throw new InvalidPasswordException("잘못된 비밀번호 입니다.", ErrorCode.INVALID_PASSWORD);
         }
+
         String token = jwtTokenProvider.createToken(user.getId(), null);
         return new LoginSuccessResponse(token);
     }
@@ -50,5 +48,4 @@ public class AuthService {
         User user = (User) authentication.getDetails();
         return user.getId();
     }
-
 }
