c9fff780653ec8bbeba66eec6d91eed491826520
jmjmjames <chungjm0711@gmail.com>
2022-09-20 22:45:25 +0900

    [Feat] SessionUser (#9)

--- src/main/java/com/ssu/travel/security/dto/SessionUser.java
+++ src/main/java/com/ssu/travel/security/dto/SessionUser.java
@@ -1,14 +1,15 @@
-package com.ssu.travel.security.model;
+package com.ssu.travel.security.dto;
 
 import lombok.Builder;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
 @Builder
-public class SessionUser {
+public class SessionUser implements Serializable {
     private Long userId;
     private String name;
     private String profilePath;
     private String accessToken;
-
 }
