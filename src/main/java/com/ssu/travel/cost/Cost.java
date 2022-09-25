5a5b39e6e4fcf0082bcf54307a04884b59546eb7
jmjmjames <chungjm0711@gmail.com>
2022-09-25 19:50:45 +0900

    [Feat] 여행에서 파생된 비용 entity (#10) - 비용은 누가 결제했는지, 또한 전체 금액에 대한 내용 - 하나의 여행을 여러 개의 비용이 존재한다. - User와 Cost는 다대다의 관계이므로 UserCost로 뽑아내야된다

--- src/main/java/com/ssu/travel/cost/Cost.java
+++ src/main/java/com/ssu/travel/cost/Cost.java
@@ -12,14 +12,11 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Cost {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "cost_id")
     private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="travel_id")
-    private Travel travel;
 
     private Long totalAmount;
 
@@ -30,21 +27,38 @@ public class Cost {
     @Column(columnDefinition = "TEXT")
     private String content;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="travel_id")
+    private Travel travel;
+
     @OneToMany(mappedBy = "cost", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<UserCost> userCosts = new ArrayList<>();
-
-    public static Cost of(Travel travel, Long totalAmount, Long payerId, String title, String content) {
-        Cost cost = new Cost();
-        cost.travel = travel;
-        cost.totalAmount = totalAmount;
-        cost.payerId = payerId;
-        cost.title = title;
-        cost.content = content;
-        return cost;
-    }
 
     public void addUserCost(UserCost userCost) {
         this.userCosts.add(userCost);
     }
 
+    public void removeUserCost(UserCost userCost) {
+        this.userCosts.remove(userCost);
+    }
+
+    public void update(Long totalAmount, Long payerId, String title, String content) {
+        this.totalAmount = totalAmount;
+        this.payerId = payerId;
+        this.title = title;
+        this.content = content;
+    }
+
+
+    private Cost(Travel travel, Long totalAmount, Long payerId, String title, String content) {
+        this.travel = travel;
+        this.totalAmount = totalAmount;
+        this.payerId = payerId;
+        this.title = title;
+        this.content = content;
+    }
+
+    public static Cost of(Travel travel, Long totalAmount, Long payerId, String title, String content) {
+        return new Cost(travel, totalAmount, payerId, title, content);
+    }
 }
--- src/main/java/com/ssu/travel/cost/CostRepository.java
+++ src/main/java/com/ssu/travel/cost/CostRepository.java
@@ -1,5 +1,6 @@
 package com.ssu.travel.cost;
 
+import com.ssu.travel.travel.Travel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,9 +9,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CostRepository extends JpaRepository<Cost, Long> {
-    @Query("select distinct c from Cost c join c.travel t join fetch c.userCosts where t.id = :travelId")
-    List<Cost> findCostsByTravelId(@Param("travelId") Long travelId);
+
+    @Query("select distinct c from Cost c join c.travel t join fetch c.userCosts where t = :travel")
+    List<Cost> findAllByTravel(@Param("travel") Travel travel);
 
     @Query("select distinct c from Cost c join fetch c.userCosts uc join fetch uc.user where c.id = :costId")
-    Optional<Cost> getCostById(@Param("costId") Long costId);
+    Optional<Cost> findCostWithUserByCostId(@Param("costId") Long costId);
 }
