25ad48a9c9a143ddc353cf6d20bab552adf7084c
jmjmjames <chungjm0711@gmail.com>
2022-09-26 23:57:46 +0900

    [Feat] Schedule (#10)

---
+++ src/main/java/com/ssu/travel/schedule/Schedule.java
@@ -0,0 +1,85 @@
+package com.ssu.travel.schedule;
+
+import com.ssu.travel.branch.Branch;
+import com.ssu.travel.place.Place;
+import com.ssu.travel.post.Post;
+import com.ssu.travel.schedulePhoto.SchedulePhoto;
+import com.ssu.travel.travel.travelDate.TravelDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "schedule_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumns({
+            @JoinColumn(name = "date"),
+            @JoinColumn(name = "travel_id")
+    })
+    private TravelDate travelDate;
+
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "travel_id")
+//    private Travel travel;
+
+    @OneToMany(mappedBy = "schedule", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Branch> branches = new ArrayList<>();
+
+    @OneToMany(mappedBy = "schedule", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SchedulePhoto> photos = new ArrayList<>();
+
+    @OneToMany(mappedBy = "schedule", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
+
+    @Setter
+    private LocalTime startTime;
+
+    @Setter
+    private LocalTime endTime;
+
+    public void addUser(Branch branch) {
+        branch.updateSchedule(this);
+    }
+
+    public void removeUser(Branch branch) {
+        this.branches.remove(branch);
+    }
+
+    public void addSchedulePhotos(List<SchedulePhoto> photos) {
+        this.photos.addAll(photos);
+    }
+
+    public void removeSchedulePhotos(List<SchedulePhoto> photos) {
+        this.photos.removeAll(photos);
+    }
+
+
+    private Schedule(TravelDate travelDate, Place place, LocalTime startTime, LocalTime endTime) {
+        this.travelDate = travelDate;
+        this.place = place;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public static Schedule of(TravelDate travelDate, Place place, LocalTime startTime, LocalTime endTime) {
+        return new Schedule(travelDate, place, startTime, endTime);
+    }
+}
--- src/main/java/com/ssu/travel/schedule/ScheduleRepository.java
+++ src/main/java/com/ssu/travel/schedule/ScheduleRepository.java
@@ -12,7 +12,10 @@ import java.util.Optional;
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     @Query("select s from Schedule s join fetch s.place where s.id = :scheduleId")
-    Optional<Schedule> findScheduleWithPlaceByScheduleId(@Param("scheduleId") Long scheduleId);
+    Optional<Schedule> findWithPlaceByScheduleId(@Param("scheduleId") Long scheduleId);
+
+    @Query("select distinct s from Schedule s left join fetch s.place left join s.travelDate td where td.date = :date and td.travel.id = :travelId")
+    List<Schedule> findAllWithPlaceByDateAndTravelId(@Param("date") LocalDate date, @Param("travelId") Long travelId);
 
 //    @Query("select distinct s from Schedule s left join fetch s.place left join s.travelDate td where td.travel = :travel and td.date = :date")
 //    List<Schedule> findAllWithPlaceByDateAndTravelId(@Param("date") LocalDate date, @Param("travel") Travel travel);
