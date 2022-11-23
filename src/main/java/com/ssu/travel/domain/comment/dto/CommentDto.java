f481ce9bc92eada6bd3485d1da98846cbcbcec60
jmjmjames <chungjm0711@gmail.com>
2022-11-23 21:26:21 +0900

    feat: Entity 와 Dto통해 계층 분리 (#17)

---
+++ src/main/java/com/ssu/travel/domain/comment/dto/CommentDto.java
@@ -0,0 +1,23 @@
+package com.ssu.travel.domain.comment.dto;
+
+import com.ssu.travel.domain.comment.entity.Comment;
+import com.ssu.travel.domain.user.dto.SimpleUserDto;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentDto {
+    private Long commentId;
+    private SimpleUserDto userInfo;
+    private String content;
+
+    public static CommentDto from(Comment comment) {
+        CommentDto commentDto = new CommentDto();
+        commentDto.commentId = comment.getId();
+        commentDto.userInfo = SimpleUserDto.from(comment.getUser());
+        commentDto.content = comment.getContent();
+        return commentDto;
+    }
+}
