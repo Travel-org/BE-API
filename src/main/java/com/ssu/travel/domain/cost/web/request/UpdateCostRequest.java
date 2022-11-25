5d95da22e3aeab94f59912cd3dfcdcdfccb370ff
jmjmjames <chungjm0711@gmail.com>
2022-11-25 15:59:43 +0900

    refactor: cost request dto field variables 변경 및 추가 (#21)

--- src/main/java/com/ssu/travel/domain/cost/web/request/UpdateCostRequest.java
+++ src/main/java/com/ssu/travel/domain/cost/web/request/UpdateCostRequest.java
@@ -1,30 +1,24 @@
-package com.ssu.travel.cost.dto.request;
+package com.ssu.travel.domain.cost.web.request;
 
+import java.util.Map;
+import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
-import java.util.Map;
-
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-public class CostCreateRequest {
+@AllArgsConstructor(staticName = "of")
+public class UpdateCostRequest {
+
     @NotNull(message = "총액을 입력해주세요.")
     private Long totalAmount;
-
-    @NotNull(message = "여행 아이디를 입력해주세요.")
-    private Long travelId;
 
     @NotNull(message = "제목을 입력해주세요.")
     private String title;
 
     private String content;
-
-    @NotNull(message = "일괄 정산 여부가 필요합니다.")
-    private Boolean isEquallyDivided;
 
     @NotNull(message = "사용자 별 금액에 대한 정보가 필요합니다.")
     private Map<Long, Long> amountsPerUser;
