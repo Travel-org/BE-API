e697a310574d2542ca0a6347891cf32ae924a61d
jmjmjames <chungjm0711@gmail.com>
2022-11-26 17:29:16 +0900

    feat: 스케줄 수정하는 기능 추가(#19)

--- src/main/java/com/ssu/travel/domain/schedule/web/ScheduleController.java
+++ src/main/java/com/ssu/travel/domain/schedule/web/ScheduleController.java
@@ -3,14 +3,20 @@ package com.ssu.travel.domain.schedule.web;
 import com.ssu.travel.domain.schedule.dto.ScheduleDto;
 import com.ssu.travel.domain.schedule.service.ScheduleService;
 import com.ssu.travel.domain.schedule.web.request.CreateScheduleRequest;
+import com.ssu.travel.domain.schedule.web.request.UpdateScheduleRequest;
 import com.ssu.travel.global.dto.Result;
 import java.time.LocalDate;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,6 +32,13 @@ public class ScheduleController {
         return ResponseEntity.ok(Result.createSuccessResult(scheduleDto));
     }
 
+    @PutMapping("/api/travel/{travelId}/schedule/{scheduleId}")
+    public ResponseEntity updateSchedule(@PathVariable Long travelId,
+                                         @PathVariable Long scheduleId,
+                                         @RequestBody @Validated UpdateScheduleRequest request) {
+        ScheduleDto scheduleDto = scheduleService.updateSchedule(scheduleId, request);
+        return ResponseEntity.ok(Result.createSuccessResult(scheduleDto));
+    }
 
     @GetMapping("/api/travel/{travelId}/schedule/{scheduleId}")
     public ResponseEntity getSchedule(@PathVariable Long travelId, @PathVariable Long scheduleId) {
