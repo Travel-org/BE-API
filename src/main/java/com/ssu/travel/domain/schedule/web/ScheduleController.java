8af7ec35ced67389d2c8b716fc118e6205e9f8f7
jmjmjames <chungjm0711@gmail.com>
2022-11-26 15:20:19 +0900

    feat: 스케줄 추가하는 기능 추가(#19)

--- src/main/java/com/ssu/travel/domain/schedule/web/ScheduleController.java
+++ src/main/java/com/ssu/travel/domain/schedule/web/ScheduleController.java
@@ -2,9 +2,14 @@ package com.ssu.travel.domain.schedule.web;
 
 import com.ssu.travel.domain.schedule.dto.ScheduleDto;
 import com.ssu.travel.domain.schedule.service.ScheduleService;
+import com.ssu.travel.domain.schedule.web.request.CreateScheduleRequest;
 import com.ssu.travel.global.dto.Result;
+import java.time.LocalDate;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,6 +17,15 @@ import org.springframework.web.bind.annotation.*;
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
+
+    @PostMapping("/api/travel/{travelId}/schedule")
+    public ResponseEntity saveSchedule(@PathVariable Long travelId,
+                                       @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
+                                       @RequestBody @Validated CreateScheduleRequest request) {
+        ScheduleDto scheduleDto = scheduleService.saveSchedule(travelId, date, request);
+        return ResponseEntity.ok(Result.createSuccessResult(scheduleDto));
+    }
+
 
     @GetMapping("/api/travel/{travelId}/schedule/{scheduleId}")
     public ResponseEntity getSchedule(@PathVariable Long travelId, @PathVariable Long scheduleId) {
