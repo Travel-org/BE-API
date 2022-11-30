5a23ebef171f85330f8b5f182f831f462675b57a
jmjmjames <chungjm0711@gmail.com>
2022-11-30 19:13:44 +0900

    chore: post 엔티티 패키지 이동 (#29)

--- src/main/java/com/ssu/travel/domain/post/entity/Post.java
+++ src/main/java/com/ssu/travel/domain/post/entity/Post.java
@@ -1,19 +1,32 @@
-package com.ssu.travel.post;
+package com.ssu.travel.domain.post.entity;
 
 
-import com.ssu.travel.comment.Comment;
-import com.ssu.travel.common.auditing.BaseTimeEntity;
-import com.ssu.travel.photo.Photo;
-import com.ssu.travel.schedule.Schedule;
-import com.ssu.travel.user.User;
-import lombok.*;
-
-import javax.persistence.*;
+import com.ssu.travel.domain.comment.entity.Comment;
+import com.ssu.travel.domain.photo.entity.Photo;
+import com.ssu.travel.domain.schedule.entity.Schedule;
+import com.ssu.travel.domain.user.entity.User;
+import com.ssu.travel.global.entity.BaseTimeEntity;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "post")
 @Entity
 public class Post extends BaseTimeEntity {
 
@@ -21,6 +34,11 @@ public class Post extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "post_id")
     private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "schedule_id")
@@ -30,27 +48,23 @@ public class Post extends BaseTimeEntity {
     @JoinColumn(name = "user_id")
     private User user;
 
-    private String title;
-
-    @Column(columnDefinition = "TEXT")
-    private String content;
-
     @OneToMany(mappedBy = "post", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
 
     @OneToMany(mappedBy = "post", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Photo> photos = new ArrayList<>();
 
+
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
     }
 
-    private Post(Schedule schedule, User user, String title, String content) {
-        this.schedule = schedule;
-        this.user = user;
+    private Post(String title, String content, Schedule schedule, User user) {
         this.title = title;
         this.content = content;
+        this.schedule = schedule;
+        this.user = user;
     }
 
     public static Post of(Schedule schedule, User user, String title, String content) {
